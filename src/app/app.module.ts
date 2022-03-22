1e60b8282e1f2875b877945041032058bf1461d3
Thiago Pio <thiago.pio2017@gmail.com>
2022-03-22 17:09:17 -0300

    hdb-name component, layout refactory in home component and refactory in parabueins word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HappyBirthdayComponent } from './pages/home/happy-birthday/happy-birthday.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HbdNameComponent } from './pages/home/hbd-name/hbd-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HappyBirthdayComponent
+    HappyBirthdayComponent,
+    HbdNameComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/home/hbd-name/hbd-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hbd-name',
+  templateUrl: './hbd-name.component.html',
+  styleUrls: ['./hbd-name.component.scss']
+})
+export class HbdNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
