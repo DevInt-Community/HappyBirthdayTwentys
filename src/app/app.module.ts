35943d301b5ced39d1a154d61c7976e2adfcbbd7
Thiago Pio <thiago.pio2017@gmail.com>
2022-03-22 17:05:41 -0300

    install bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HappyBirthdayComponent } from './pages/home/happy-birthday/happy-birthday.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HappyBirthdayComponent } from './pages/home/happy-birthday/happy-birthd
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
