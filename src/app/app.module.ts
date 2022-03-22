63007cca0fc23320f3a69b72ea17210de4c82e06
Thiago Pio <thiago.pio2017@gmail.com>
2022-03-22 16:34:35 -0300

    happy-birthday component and your settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { HappyBirthdayComponent } from './pages/home/happy-birthday/happy-birthday.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HappyBirthdayComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/happy-birthday/happy-birthday.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-happy-birthday',
+  templateUrl: './happy-birthday.component.html',
+  styleUrls: ['./happy-birthday.component.scss']
+})
+export class HappyBirthdayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
